2298797a33a963bb06c4e55ae64e36f6357988ba
PoAn (Baron) Chen <chen.baron@hotmail.com>
2017-08-14 18:30:32 -0700

    Create different.java

---
+++ different/different.java
@@ -0,0 +1,24 @@
+// Problem link: https://open.kattis.com/problems/different
+// Live example link: https://repl.it/KJxu
+// CPU running time: 0.11 s
+// Status: Accepted
+
+import java.util.*;
+
+public class different {
+  public static void main(String [] args) {
+    Scanner scanner = new Scanner(System.in);
+    String nextLine = "";
+    
+    while(scanner.hasNextLine()) {
+      nextLine = scanner.nextLine();
+      
+      System.out.println(
+        Math.abs(
+          Long.parseLong(nextLine.split(" ")[0]) -
+          Long.parseLong(nextLine.split(" ")[1])
+        )
+      );
+    }
+  }
+}
